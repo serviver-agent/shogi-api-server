073bc8e0db40cbdc7ab553dad3ce2988e090174d
KazuyaMiayshita <miy1924@gmail.com>
2020-09-19 00:35:08 +0900

    JSON周りの修正2

--- src/main/scala/web/ShogiMock.scala
+++ src/main/scala/web/ShogiMock.scala
@@ -31,43 +31,39 @@ class ShogiMock {
     import core._
     import io.circe._
 
-    val komaEncoder: Encoder[Koma] = new Encoder[Koma] {
-      override def apply(a: Koma): Json = a match {
-        case Koma.Lion   => Json.fromString("Lion")
-        case Koma.Kirin  => Json.fromString("Kirin")
-        case Koma.Zou    => Json.fromString("Zou")
-        case Koma.Hiyoko => Json.fromString("Hiyoko")
-      }
+    val komaEncoder: Encoder[Koma] = Encoder.instance {
+      case Koma.Lion   => Json.fromString("Lion")
+      case Koma.Kirin  => Json.fromString("Kirin")
+      case Koma.Zou    => Json.fromString("Zou")
+      case Koma.Hiyoko => Json.fromString("Hiyoko")
     }
 
-    val locationEncoder: Encoder[Location] = new Encoder[Location] {
-      override def apply(a: Location): Json = a match {
-        case Location.OnPlayer1Tegoma =>
-          Json.obj(
-            "tpe" -> Json.fromString("onPlayer1Tegoma")
-          )
-        case Location.OnPlayer2Tegoma =>
-          Json.obj(
-            "tpe" -> Json.fromString("onPlayer1Tegoma")
-          )
-        case Location.OnShogiban(yoko, tate) =>
-          Json.obj(
-            "tpe"  -> Json.fromString("onShogiban"),
-            "yoko" -> Json.fromInt(yoko.n),
-            "tate" -> Json.fromInt(tate.n)
-          )
-      }
+    val locationEncoder: Encoder[Location] = Encoder.instance {
+      case Location.OnPlayer1Tegoma =>
+        Json.obj(
+          "tpe" -> Json.fromString("onPlayer1Tegoma")
+        )
+      case Location.OnPlayer2Tegoma =>
+        Json.obj(
+          "tpe" -> Json.fromString("onPlayer1Tegoma")
+        )
+      case Location.OnShogiban(yoko, tate) =>
+        Json.obj(
+          "tpe"  -> Json.fromString("onShogiban"),
+          "yoko" -> Json.fromInt(yoko.n),
+          "tate" -> Json.fromInt(tate.n)
+        )
     }
 
-    val komaStatusEncoder: Encoder[KomaStatus] = new Encoder[KomaStatus] {
-      override def apply(a: KomaStatus): Json = Json.obj(
+    val komaStatusEncoder: Encoder[KomaStatus] = Encoder.instance { a =>
+      Json.obj(
         "koma"     -> komaEncoder(a.koma),
         "loaction" -> locationEncoder(a.location)
       )
     }
 
-    val shogibanEncoder: Encoder[Shogiban] = new Encoder[Shogiban] {
-      override def apply(a: Shogiban): Json = Json.obj(
+    val shogibanEncoder: Encoder[Shogiban] = Encoder.instance { a =>
+      Json.obj(
         "lion1"   -> komaStatusEncoder(a.lion1),
         "lion2"   -> komaStatusEncoder(a.lion2),
         "kirin1"  -> komaStatusEncoder(a.kirin1),
@@ -79,16 +75,14 @@ class ShogiMock {
       )
     }
 
-    val shiaiStatusEncoder: Encoder[Shiai.ShiaiStatus] = new Encoder[Shiai.ShiaiStatus] {
-      override def apply(a: Shiai.ShiaiStatus): Json = a match {
-        case Shiai.ShiaiStatus.Player1 => Json.fromString("Player1")
-        case Shiai.ShiaiStatus.Player2 => Json.fromString("Player2")
-        case Shiai.ShiaiStatus.Finish  => Json.fromString("Finish")
-      }
+    val shiaiStatusEncoder: Encoder[Shiai.ShiaiStatus] = Encoder.instance {
+      case Shiai.ShiaiStatus.Player1 => Json.fromString("Player1")
+      case Shiai.ShiaiStatus.Player2 => Json.fromString("Player2")
+      case Shiai.ShiaiStatus.Finish  => Json.fromString("Finish")
     }
 
-    val shiaiEncoder: Encoder[Shiai] = new Encoder[Shiai] {
-      override def apply(a: Shiai): Json = Json.obj(
+    val shiaiEncoder: Encoder[Shiai] = Encoder.instance { a =>
+      Json.obj(
         "shogiban"    -> shogibanEncoder(a.shogiban),
         "shiaiStatus" -> shiaiStatusEncoder(a.shiaiStatus)
       )
