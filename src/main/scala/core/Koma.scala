f561e3c8ae27285c0deef060a20b368419c26b50
KazuyaMiayshita <miy1924@gmail.com>
2020-09-25 07:24:34 +0900

    相手のにわとりを取ったら手駒のひよこにする

--- src/main/scala/core/Koma.scala
+++ src/main/scala/core/Koma.scala
@@ -13,6 +13,7 @@ sealed trait Koma {
     case Gote  => relativeAreaSeenFromSente.map { case RelativeArea(x, y) => RelativeArea(-x, -y) }
   }
   def nari: Option[Koma]
+  def nariModori: Koma
 
   final def owned(player: Player): PlayersKoma = PlayersKoma(this, player)
 
@@ -23,21 +24,26 @@ object Koma {
     override protected val relativeAreaSeenFromSente: Set[RelativeArea] =
       Set(Up, Right, Down, Left, RightUp, RightDown, LeftUp, LeftDown)
     override def nari: Option[Koma] = None
+    override def nariModori: Koma   = this
   }
   case object Kirin extends Koma {
     override protected val relativeAreaSeenFromSente: Set[RelativeArea] = Set(Up, Right, Down, Left)
     override def nari: Option[Koma]                                     = None
+    override def nariModori: Koma                                       = this
   }
   case object Zou extends Koma {
     override protected val relativeAreaSeenFromSente: Set[RelativeArea] = Set(RightUp, RightDown, LeftUp, LeftDown)
     override def nari: Option[Koma]                                     = None
+    override def nariModori: Koma                                       = this
   }
   case object Hiyoko extends Koma {
     override protected val relativeAreaSeenFromSente: Set[RelativeArea] = Set(Up)
     override def nari: Option[Koma]                                     = Some(Niwatori)
+    override def nariModori: Koma                                       = this
   }
   case object Niwatori extends Koma {
     override protected val relativeAreaSeenFromSente: Set[RelativeArea] = Set(Up, Right, Down, Left, RightUp, RightDown)
     override def nari: Option[Koma]                                     = None
+    override def nariModori: Koma                                       = Hiyoko
   }
 }
